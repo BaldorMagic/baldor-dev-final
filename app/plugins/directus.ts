619d749dedfa4ec92941972dbce127818dd4ddc5
Baldor <james@baldor.dev>
2025-12-10 16:54:34 +0100

    directus fetching file removed

--- app/plugins/directus.ts
+++
@@ -1,9 +0,0 @@
-import { createDirectus, rest, readItems } from "@directus/sdk";
-const directus = createDirectus<DirectusSchema>(
-    "https://your-website-url/directus",
-).with(rest());
-export default defineNuxtPlugin(() => {
-    return {
-        provide: { directus, readItems },
-    };
-});
