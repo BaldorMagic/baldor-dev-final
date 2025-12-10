f3cc1d14818fbd34b5dc1562cc76074e9efdacf3
Baldor <james@baldor.dev>
2025-12-10 16:32:28 +0100

    protocol changed

--- app/plugins/directus.ts
+++ app/plugins/directus.ts
@@ -1,6 +1,6 @@
 import { createDirectus, rest, readItems } from "@directus/sdk";
 const directus = createDirectus<DirectusSchema>(
-    "http://your-website-url/directus",
+    "https://your-website-url/directus",
 ).with(rest());
 export default defineNuxtPlugin(() => {
     return {
